68133ca6813b4adc8878abd1843ed3a4ba9de72c
Nantebi1 <yanteb@aol.com>
2018-10-15 20:57:59 -0400

    Updates

---
+++ precache-manifest.3c9c5a75f2a294e9af8adc2eaf3bb789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "617e3c50e67378d4af10",
+    "url": "/robo-friends/static/js/runtime~main.617e3c50.js"
+  },
+  {
+    "revision": "f2d7ccb455a95f662798",
+    "url": "/robo-friends/static/js/main.f2d7ccb4.chunk.js"
+  },
+  {
+    "revision": "19cbb7f16102724afd32",
+    "url": "/robo-friends/static/js/1.19cbb7f1.chunk.js"
+  },
+  {
+    "revision": "f2d7ccb455a95f662798",
+    "url": "/robo-friends/static/css/main.9eb5b8ae.chunk.css"
+  },
+  {
+    "revision": "19cbb7f16102724afd32",
+    "url": "/robo-friends/static/css/1.24442c7b.chunk.css"
+  },
+  {
+    "revision": "0d8cccd3e8000283a373b65a88c82906",
+    "url": "/robo-friends/index.html"
+  }
+];
